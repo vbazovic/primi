b6e49206e722c18fdebd97ade4b9ee875e69212f
Vladimir Bazovic <vbazovic@gmail.com>
2022-06-23 13:52:21 +0200

    jwt token added

--- src/extensions/psl/CurlExtension.php
+++ src/extensions/psl/CurlExtension.php
@@ -19,22 +19,34 @@ class CurlExtension extends Extension {
      * Fetches content from $cUrl
      * 
      * @param string $cUrl
+     * @param str $header   Optional header
      * @return string|null
      */
-    public static function curl_get(string $cUrl):?string {
+    public static function curl_get(string $cUrl, array $header = []): ?string {
         $ch = curl_init($cUrl);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 4);
         curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+        if (!empty($header)) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        }
         $result = curl_exec($ch);
         if (PHP_SAPI === 'cli') {
             echo $result . "\n";
             return null;
-        } else {       
+        } else {
             return $result;
         }
+    }
+
+    public static function curl_get_jwt(string $cUrl, string $token, string $key = 'App-Key') {
+        $header = [
+            "Accept: application/json",
+            $key . ": " . $token,            
+        ];
+        return self::curl_get($cUrl, $header);
     }
 
 }
